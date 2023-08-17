fde2a77d58ee27c25342193fd07d3d22b115faf1
PAULO CESAR DE MOURA FONSECA <paulo.fonseca@a.ucb.br>
2023-08-17 11:03:01 -0300

    Atividade 1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,25 @@
+# Declaração das variáveis numéricas
+num_A = 10
+num_B = 5
+
+# Adição
+soma = num_A + num_B
+
+# Subtração
+subtracao = num_A - num_B
+
+# Multiplicação
+multiplicacao = num_A * num_B
+
+# Divisão
+divisao = num_A / num_B
+
+# Exponenciação
+exponenciacao = num_A ** num_B
+
+# Impressão dos resultados
+print(f"Resultado da adição entre {num_A} e {num_B} = {soma}")
+print(f"Resultado da subtração entre {num_A} e {num_B} = {subtracao}")
+print(f"Resultado da multiplicação entre {num_A} e {num_B} = {multiplicacao}")
+print(f"Resultado da divisão entre {num_A} e {num_B} = {divisao}")
+print(f"Resultado da exponenciação de {num_A} elevado a {num_B} = {exponenciacao}")
